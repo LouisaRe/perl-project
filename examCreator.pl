4ff40a01ff51140af4cbf712caa30aac6f13eaf4
LouisaRe <louisa.reinger@gmx.de>
2021-08-30 22:20:26 +0200

    improve createExamFile: Add Regexes, return correct answers

--- examCreator.pl
+++ examCreator.pl
@@ -27,59 +27,71 @@ createExamFile($solutionFile, $examFile);
 
 sub createExamFile($fromFile, $toFile){
     
-    open(my $ff, '<', $fromFile)     or die "$fromFile: $!";
-    open(my $tf, '>', $toFile  )     or die "$toFile: $!";
+    open(my $ff, '<', $fromFile)    or die "$fromFile: $!";
+    open(my $tf, '>', $toFile  ) or die "$toFile: $!";
 
-    my $questionNumber = 1;
-    my %currentAnswerSet;
-    my $introText = 1;
+    state $questionNumber = 1;
+    state $answerNumber = 1;
+    state %currentAnswerSet;
+    state @allCorrectAnswers;
+    state $introText = 1;
+
+    state $LINE_REGEX   = qr{ ^ _+ $ }xms;
+    state $ANSWER_REGEX = qr{ ^ \s* \[ .* \] }xms;
 
     while(my $line = readline()){
-
         #intro text
         if($introText){
             print({$tf} $line);
-            if(index($line, "_") == 0){ #end of intro-section
+            if($line =~ $LINE_REGEX){ #end of intro-section
                 $introText = 0;
             }
         }
 
         #read and save new answer
-        elsif(index($line, "[") >= 0){
-            readAndSaveNewAnswer(\%currentAnswerSet, \$line)
+        elsif($line =~ $ANSWER_REGEX){
+            readAndSaveNewAnswer(\%currentAnswerSet, \$line, \$answerNumber, \@allCorrectAnswers);
         }
 
         #print lines
         else{
             printAllPossibleAnswers(\%currentAnswerSet, $tf);
             print({$tf} $line);
-            if(index($line, "_") == 0){ #end of current question-section
+            if($line =~ $LINE_REGEX){ #end of current question-section
                 $questionNumber++;
+                $answerNumber = 1;
             }
         }
     }
 
     close($ff);
     close($tf);
+
+    return @allCorrectAnswers;
 }
 
 
 sub printAllPossibleAnswers($currentAnswerSet_ref, $toFile_ref){
-    if(keys(%{$currentAnswerSet_ref}) != 0){ 
-        for my $b (keys %{$currentAnswerSet_ref}){
-            say({$toFile_ref} "\t[ ] ${$currentAnswerSet_ref}{$b}");
+    if(keys(%{$currentAnswerSet_ref}) != 0){
+        for my $key (keys %{$currentAnswerSet_ref}){
+            say({$toFile_ref} "\t[ ] ${$currentAnswerSet_ref}{$key}");
         }
         %{$currentAnswerSet_ref} = ();
     }
 }
 
-sub readAndSaveNewAnswer($currentAnswerSet_ref, $line_ref){
-    state $answerNumber = 0;
-
+sub readAndSaveNewAnswer($currentAnswerSet_ref, $line_ref, $answerNumber_ref, $allCorrectAnswers_ref){
     my $bracketsStart   = index(${$line_ref}, "[");
     my $bracketsEnd     = index(${$line_ref}, "]");
     my $bracketSize     = $bracketsEnd - $bracketsStart + 1;
+    my $bracket         = trim(substr(${$line_ref}, $bracketsStart, $bracketSize));
     my $answer          = trim(substr(${$line_ref}, $bracketsEnd+1)); #https://metacpan.org/pod/Text::Trim
+    
+    state $CORRECT_ANSWER_REGEX = qr{ ^ \[ \S+ \] }xms;
+    
+    if($bracket =~ $CORRECT_ANSWER_REGEX){ # save correct answer
+        push(@{$allCorrectAnswers_ref}, $answer);
+    }
 
-    ${$currentAnswerSet_ref}{"answer ".$answerNumber++} = $answer;
+    ${$currentAnswerSet_ref}{"answer".${$answerNumber_ref}++} = $answer;
 }
