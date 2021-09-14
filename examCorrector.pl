6cfefe337e8fb29b6e4dcd715757c2986a85cacb
LouisaRe <louisa.reinger@gmx.de>
2021-09-14 19:23:00 +0200

    report possible misconduct and better positioning of output

--- examCorrector.pl
+++ examCorrector.pl
@@ -9,7 +9,8 @@ use experimental 'signatures';
 use Data::Show;
 use Text::Trim;
 use Text::Levenshtein qw(distance);
-use Lingua::StopWords qw( getStopWords );
+use Lingua::StopWords qw(getStopWords);
+use Array::Utils qw(:all);
 
 use lib "/Users/louisa/fhnw/perl/final_project/lib";
 use Util::IO;
@@ -20,16 +21,18 @@ use Util::IO;
 
 #files
 my ($solutionFile, @examFiles)                  = @ARGV;
+
+#read & check all files
 my ($examFileLines_ref, $solutionAllQAs_ref)    = readFile($solutionFile, 0);
+my ($examResults_ref, $wrongAnsweredQ_ref)      = checkExamFiles();
+my $lengthLongestFileName                       = calculateLongestFileName(@examFiles);
 
-my $examResults_ref = checkExamFiles();
-
-reportResults           (scalar(keys %{$solutionAllQAs_ref}), $examResults_ref);
+#reports
+reportResults           (scalar(keys %{$solutionAllQAs_ref}), $examResults_ref, $lengthLongestFileName);
 reportCohortPerformence (scalar(keys %{$solutionAllQAs_ref}), $examResults_ref);
 reportMissingElements   ($examResults_ref);
-reportNotExpected       (scalar(keys %{$solutionAllQAs_ref}), $examResults_ref);
-
-
+reportNotExpected       (scalar(keys %{$solutionAllQAs_ref}), $examResults_ref, $lengthLongestFileName);
+reportPossibleMisconduct($wrongAnsweredQ_ref, scalar( %{ %{$solutionAllQAs_ref}{"section1"} -> {"answers"}}), $lengthLongestFileName);
 
 ################################################################################
 #Subroutines
@@ -37,6 +40,7 @@ reportNotExpected       (scalar(keys %{$solutionAllQAs_ref}), $examResults_ref);
 sub checkExamFiles(){
 
     my $examResults_ref;
+    my $wrongAnsweredQ_ref;
 
     FILE:
     for my $file (@examFiles){
@@ -57,16 +61,16 @@ sub checkExamFiles(){
 
             # fill missed elements array
             if($minQDistance > 0){
-                push($examResults_ref -> {"missedEl"} -> {$file} -> @* , "Section $sectNrSol - Missing question \t: $solQ");
+                push($examResults_ref -> {"missedEl"} -> {$file} -> @* , "Section $sectNrSol - Missing question \t\t: $solQ");
                 if($minQDistance/length($solQNormalized) <= 0.1){
-                    push($examResults_ref -> {"missedEl"} -> {$file} -> @* , "Section $sectNrSol - Used instead \t: ${$bestFitSection}{'question'}");
+                    push($examResults_ref -> {"missedEl"} -> {$file} -> @* , "Section $sectNrSol - Used instead \t\t: ${$bestFitSection}{'question'}");
                 }
             }
 
             ##############################################
             #Answer Check
 
-            # only look at answers of relevant questions (distance not more than 10% of the length)
+            # only look at answers of relevant questions (distance no more than 10% of the length)
             if($minQDistance/length($solQNormalized) <= 0.1){ 
 
                 my %solAOfCurrQ      = % {%{ %{$solutionAllQAs_ref}{"section$sectNrSol"} }{"answers"}};
@@ -82,16 +86,21 @@ sub checkExamFiles(){
 
                     # fill missed elements array
                     if($minADistance > 0){
-                        push($examResults_ref -> {"missedEl"} -> {$file} -> @* , "Section $sectNrSol - Missing answer \t: $sa\n");
+                        push($examResults_ref -> {"missedEl"} -> {$file} -> @* , "Section $sectNrSol - Missing answer \t\t: $sa\n");
                         if($minADistance/length($solANormalized) <= 0.1){
-                            push($examResults_ref -> {"missedEl"} -> {$file} -> @* , "Section $sectNrSol - Used instead \t: $bestFitA\n");
+                            push($examResults_ref -> {"missedEl"} -> {$file} -> @* , "Section $sectNrSol - Used instead \t\t: $bestFitA\n");
                         }
                     }
                     
                     # Is the correct answer checked?
                     if($minADistance/length($solANormalized) <= 0.1){ 
+                        # set correct answer checked flag
                         if($solAOfCurrQ{$sa} == 1 && $examAOfCurrQ{$bestFitA} == 1){
                             $correctAChecked = 1;
+                        }
+                        # save wrongly checked answer
+                        if($solAOfCurrQ{$sa} == 0 && $examAOfCurrQ{$bestFitA} == 1){
+                            push( $wrongAnsweredQ_ref -> {$file} -> {$sectNrSol} -> @*, $sa);
                         }
                     }
                 }
@@ -120,7 +129,7 @@ sub checkExamFiles(){
         ${$examResults_ref}{"answeredQ"}{"total"}  += $answeredQCounter;       
     }
 
-    return $examResults_ref;
+    return ($examResults_ref, $wrongAnsweredQ_ref);
 }
 
 
@@ -212,13 +221,13 @@ sub calculateDistance($solText, $examText){
 ################################################################################
 #report subroutines
 
-sub reportResults($totalQuestions, $examResults_ref){
+sub reportResults($totalQuestions, $examResults_ref, $lengthLongestFileName){
 
     say("\n________RESULTS________\n");
 
     # print results
     for my $file (@examFiles){
-        say($file." \t: ".${$examResults_ref}{'correctAns'}{$file}."/$totalQuestions");
+        say(equalLength($file, $lengthLongestFileName)." \t: ".${$examResults_ref}{'correctAns'}{$file}."/$totalQuestions");
     }
 }
 
@@ -281,9 +290,9 @@ sub reportCohortPerformence($totalQuestions, $examResults_ref){
 
 # Print out all exams that have a grade < 3.75, and so didn't pass the test.
 # In addition print all passed exams that are in the bottom 25% of all exams.
-sub reportNotExpected($totalQuestions, $examResults_ref){
+sub reportNotExpected($totalQuestions, $examResults_ref, $lengthLongestFileName){
 
-    say("\n________BELOW EXPECTATION________ \n");
+    say("________BELOW EXPECTATION________ \n");
 
     my $nrBottom25 = sprintf('%.0f' , (scalar(@examFiles) / 4)) // 1;
     my @lowestResults = ();
@@ -296,7 +305,7 @@ sub reportNotExpected($totalQuestions, $examResults_ref){
     for my $file (@examFiles){
         my $grade = ${$examResults_ref}{'correctAns'}{$file}/$totalQuestions * 5 + 1;
         if($grade < 3.75){
-            say( "$file \t: ${$examResults_ref}{'correctAns'}{$file}/$totalQuestions (not passed -> reached grade: " . sprintf("%.2f", $grade) . ")" );
+            say( equalLength($file, $lengthLongestFileName) ." \t: ${$examResults_ref}{'correctAns'}{$file}/$totalQuestions (not passed -> reached grade: " . sprintf("%.2f", $grade) . ")" );
         }else{
             for my $lowR (@lowestResults){
                 if($lowR == ${$examResults_ref}{'correctAns'}{$file}){
@@ -316,4 +325,97 @@ sub reportMissingElements($examResults_ref){
             say $examResults_ref -> {"missedEl"} -> {$file} -> @*;
         }
     }
+}
+
+sub reportPossibleMisconduct($wrongAnsweredQ_ref, $nrOfAPerQ, $lengthLongestFileName){
+    say("\n________POSSIBLE ACADEMIC MISCONDUCT________ \nnumber of same wrong answers (cheating probability in %)\n");
+    
+    my $remainingWrongAnsweredQ_ref = {%$wrongAnsweredQ_ref};
+    my $noCheating = 1;
+    my $nrOfSameWrongA = 0;
+
+    CURRENT_FILE:
+    for my $currFile ( keys %{$wrongAnsweredQ_ref} ){
+
+        delete($remainingWrongAnsweredQ_ref -> {$currFile});
+
+        COMPAIR_FILE:
+        for my $compairFile ( keys %{$remainingWrongAnsweredQ_ref} ){
+
+            $nrOfSameWrongA = 0;
+            
+            CURRENT_SECTION:
+            for my $currSection ( keys % {$wrongAnsweredQ_ref -> {$currFile}} ){
+
+
+                COMPAIR_SECTION:
+                for my $compairSection ( keys % {$remainingWrongAnsweredQ_ref -> {$compairFile}} ){
+
+                    if($currSection eq $compairSection){
+
+                        if( !array_diff($wrongAnsweredQ_ref -> {$currFile} -> {$currSection} -> @*, $remainingWrongAnsweredQ_ref -> {$compairFile} -> {$compairSection} -> @*) ) {
+                            $nrOfSameWrongA++;
+                            $noCheating = 0;
+                        }
+                    }
+                }
+            }
+
+            my $nCurrent = scalar( % {$wrongAnsweredQ_ref -> {$currFile}} );
+            my $nCompair = scalar (% {$remainingWrongAnsweredQ_ref -> {$compairFile}} );
+            my $x = $nrOfSameWrongA; 
+            my $p = 1/($nrOfAPerQ-1);
+
+            if($nrOfSameWrongA != 0){
+                say("     ".equalLength($currFile, $lengthLongestFileName) ."\t $nrOfSameWrongA (". calculateCheatingProbability($x, $nCurrent, $p) ."%)\n"
+                ." and ".equalLength($compairFile, $lengthLongestFileName) ."\t $nrOfSameWrongA (". calculateCheatingProbability($x, $nCompair, $p) ."%)\n");
+            }
+        }
+    }
+
+    if($noCheating){
+        say "No one seems to have cheated :)";
+    }
+}
+
+sub calculateCheatingProbability($x, $n, $p){
+    my $k = $x;
+    my $kWithMaxP = int(($n+1)*$p);
+    my $result;
+    if($k <= $kWithMaxP){
+        $result = binomialDistribution($x, $n, $p);
+    }else{
+        $result = 1-binomialDistribution($x, $n, $p);
+    }
+    return sprintf( "%.1f" ,$result*100);
+}
+
+sub binomialDistribution($x, $n, $p){
+    my $k = $x;
+    return binomial($n, $k) * ($p**$k) * ((1 - $p)**($n-$k));
+}
+
+sub binomial($n, $k) { #https://rosettacode.org/wiki/Evaluate_binomial_coefficients#Perl
+    my $r = 1;
+
+    for my $koeff (1 .. $k) { 
+        $r *= $n--; 
+        $r /= $koeff;
+    }
+    return $r;
+}
+
+sub equalLength($string, $length){
+    $string .= (" " x ($length - length($string)));
+    return $string; 
+}
+
+sub calculateLongestFileName(@examFiles){
+    my $lengthLongestName = 0;
+    for my $file (@examFiles){
+        if(length($file) > $lengthLongestName){
+            $lengthLongestName = length($file);
+        }
+    }
+    return $lengthLongestName;
 }
